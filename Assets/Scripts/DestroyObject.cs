c1a4e435bb053c1e7c5d5795eab3d85a3a80cc1a
szekelysteven <91074581+szekelysteven@users.noreply.github.com>
2021-09-22 15:53:01 -0400

    Updated Test Script Scene
    Added different buildings with different spawn rates and types of donators spawned. donaters can be clicked to add to global variable "totalmoney".

--- Assets/Scripts/DestroyObject.cs
+++ Assets/Scripts/DestroyObject.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class DestroyObject : MonoBehaviour
 {
+    
+    public float value;
     // Start is called before the first frame update
   
 
     void OnMouseDown()
     {
+        GameObject.Find("MathScript").GetComponent<MathScript>().TotalMoney += value;
         Destroy(gameObject);
     }
 
